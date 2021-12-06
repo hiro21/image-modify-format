9aa3cf10305cb1c61d01d2937893c6a92dfd2e79
hiroyuki-seto <hiswing8@gmail.com>
2021-12-06 20:41:59 +0900

    ファイル読み込み追加

--- image-modify-format.go
+++ image-modify-format.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("test")
+
+	// file read
+	f, err := os.ReadFile("/Users/setohiroyuki/work/docker/gopher/image-modify-format/image/mountain-g5a8babad2_1920.jpg")
+	if err != nil {
+		log.Fatal(err)
+
+	}
+	fmt.Print(f)
+
+	// modify
 }
